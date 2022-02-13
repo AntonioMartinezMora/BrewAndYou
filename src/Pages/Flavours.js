82ed485dccc07e35b99d5f6c3512e6db7edf4b0a
Antonio Martinez Mora <martinezmora.ant@gmail.com>
2022-02-13 13:58:37 -0500

    Functionallity for type of coffee page and test styling

--- src/Pages/Flavours.js
+++ src/Pages/Flavours.js
@@ -1,10 +1,39 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import data from '../Data.json'
+import '../Style/App.css'; 
+import '../Style/Flavours.css'
 
 function Flavours(){
     let { flavour } = useParams()
 
-    return <h2>this is page is for {flavour} coffees</h2>
+    return (
+        <div className="coffees__container">
+            {data.map((item)=>{
+                if(item.type === flavour){
+                    const {
+                        id,
+                        index,
+                        image,
+                        type,
+                        name
+                    } = item
+                    return<Link
+                        to={`/coffees/${id}`}
+                        key={index}
+                        className="coffee__link"
+                    >
+                        <div className="coffee__link--info">
+                            <img src={image}/>
+                            <h4 className="coffee__name">
+                                {name}
+                            </h4>
+                        </div>
+                    </Link>
+                }
+            })}
+        </div>
+    )
 }
 
 export default Flavours;
