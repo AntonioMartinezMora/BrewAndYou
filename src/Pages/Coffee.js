9d18477f3e2a4eefaa454673afe1b6eb5a49db74
Antonio Martinez Mora <martinezmora.ant@gmail.com>
2022-02-20 17:54:51 -0500

    Coffee needs section created & styled

--- src/Pages/Coffee.js
+++ src/Pages/Coffee.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { AiOutlineConsoleSql } from "react-icons/ai";
 import { Link, useParams } from "react-router-dom";
+// import {FaChevronRight} from "react-icons/ai";
+import { AiFillCaretRight } from "react-icons/ai";
 import data from '../Data.json'
 import '../Style/App.css'; 
 import '../Style/Coffee.css'; 
@@ -20,10 +21,16 @@ function Coffee(){
                         country,
                         name,
                         descript,
-                        recipe
+                        recipe,
+                        needOne,
+                        needTwo,
+                        needThree,
+                        btnOne,
+                        btnTwo,
+                        btnThree
                     } = item
                     const newCoffee = {
-                        id, image, type, country, name, descript, recipe, index
+                        id, image, type, country, name, descript, recipe, index, needOne, needTwo, needThree, btnOne, btnTwo, btnThree
                     }
                     return(
                         <div 
@@ -52,6 +59,51 @@ function Coffee(){
                                         recipe
                                     </h3>
                                     <p>{recipe}</p> 
+                                </div>
+                            </div>
+                            <div className="coffee__needs">
+                                <h2>You will need to make this coffee:</h2>
+                                <div className="need__info">
+                                    <span>
+                                    <AiFillCaretRight />   
+                                    {needOne} 
+                                    </span>
+                                    <button className={btnOne ? "find__btn" : "find__btn hide"}>
+                                        <a 
+                                            href={btnOne}
+                                            target="_blank"
+                                        >
+                                            Find it
+                                        </a>
+                                    </button>
+                                </div>
+                                <div className="need__info">
+                                    <span>
+                                    <AiFillCaretRight />   
+                                    {needTwo} 
+                                    </span>
+                                    <button className={btnTwo ? "find__btn" : "find__btn hide"}>
+                                        <a 
+                                            href={btnTwo}
+                                            target="_blank"
+                                        >
+                                            Find it
+                                        </a>
+                                    </button>
+                                </div>
+                                <div className="need__info">
+                                    <span>
+                                    <AiFillCaretRight />   
+                                    {needThree} 
+                                    </span>
+                                    <button className={btnThree ? "find__btn" : "find__btn hide"}>
+                                        <a 
+                                            href={btnThree}
+                                            target="_blank"
+                                        >
+                                            Find it
+                                        </a>
+                                    </button>
                                 </div>
                             </div>
                         </div>
