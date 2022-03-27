ef28ca60f40c113c4ef83c044d2f0c10a215e19f
Antonio Martinez Mora <martinezmora.ant@gmail.com>
2022-03-27 18:43:45 -0400

    Hero news created

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,16 +1,74 @@
 import React from "react";
-import HeroBtn from "./HeroBtn";
+import { Link } from 'react-router-dom'
 import '../Style/App.css'
+import HeroData from '../HeroData.json'
 
 const Hero = () => {
     return (
         <div className="hero">
-            <div className="hero__info">
-                <h2>Coffee is not just a drink; it's a an experience, a culture and way to bring people together.</h2>
-                <p>Learn about coffee drinks from all over the world and how they are made.</p>
-                <HeroBtn />
+            <div className="hero__main">
+                {HeroData.map((item)=>{
+                    if(item.hero === 'main'){
+                        const {
+                            id,
+                            type,
+                            index,
+                            image,
+                            title,
+                            short
+                        } = item
+                        return(
+                        <Link
+                            to={`/${type}/${id}`}
+                            key={index}
+                            className="hero__main--link"
+                        >
+                            <img src={image} />
+                            <h6
+                               className="hero__tag" 
+                            >{type}</h6>
+                            <h3>{title}</h3>    
+                            <p>{short}</p>    
+                        </Link> 
+                        )
+                    }
+                })}
             </div>
+            <div className="hero__side">
+                {HeroData.map((item)=>{
+                    if(item.hero === 'side'){
+                        const {
+                            id,
+                            type,
+                            index,
+                            image,
+                            title,
+                            short
+                        } = item
+                        return(
+                        <Link
+                            to={`/${type}/${id}`}
+                            key={index}
+                            className="hero__side--link"
+                        >
+                            <div 
+                                className="img__cont"
+                                style={{ backgroundImage:`url(${image})` }}
+                            >   
+                            </div>
 
+                            <div className="hero__side--info">
+                                <h6
+                                className="hero__tag" 
+                                >{type}</h6>
+                                <h3>{title}</h3>    
+                                <p>{short}</p>    
+                            </div>
+                        </Link> 
+                        )
+                    }
+                })}
+            </div>
         </div>
     )
 }
