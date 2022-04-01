41f9063f9e9bcd81b3317f6600316603b2b1f3eb
Antonio Martinez Mora <martinezmora.ant@gmail.com>
2022-04-01 19:40:37 -0400

    Menu content added

--- src/Components/MenuContent.js
+++ src/Components/MenuContent.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useGlobalContext } from '../context'
+import { BiWorld, BiNews, BiCoffeeTogo,BiEnvelope } from "react-icons/bi";
+import { FaInstagramSquare,FaTwitterSquare  } from "react-icons/fa";
 
 const MenuContent = () =>{
     const {openMenu, setOpenMenu} = useGlobalContext()
@@ -10,7 +12,55 @@ const MenuContent = () =>{
         <div 
         className={openMenu ? "menu__content open" : "menu__content"}
         >
-            sup appaao
+            <Link
+                to={"/news"}
+                className="menu__link"
+            >
+                <BiNews  
+                className='menu__link--icon'/>
+                Coffee<span className="menu__span">News</span>
+            </Link>
+            <Link
+                to={"/trends"}
+                className="menu__link"
+            >
+                <BiCoffeeTogo
+                className='menu__link--icon' />
+                <span className="menu__span">Trending</span> Coffee Beverages
+            </Link>
+            <Link
+                to={"/discover"}
+                className="menu__link"
+            >
+                <BiWorld
+                className='menu__link--icon' />
+                <span className="menu__span">Discover</span> coffees of the world
+            </Link>
+
+            <Link
+                to={"/contact"}
+                className="menu__link"
+            >
+                <BiEnvelope
+                className='menu__link--icon' />
+                Contact us
+            </Link>
+
+            <div className="menu__social">
+                <FaInstagramSquare
+                    className="icon__social"
+                />
+                <FaTwitterSquare
+                    className="icon__social"
+                />
+            </div>
+
+            <div className="menu__info">
+                <h3>BrewedAndYou</h3>
+                <p>For coffee lovers and people that enjoy a coffee in any way at any time.</p>    
+            </div>
+
+
         </div>
     )
 }
