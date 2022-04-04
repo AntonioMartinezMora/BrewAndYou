52554e9a592c5d79a4082df78a8b3248938c86f2
Antonio Martinez Mora <martinezmora.ant@gmail.com>
2022-04-04 18:45:04 -0400

    Search container functionallity created and styled

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,33 +1,91 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react/cjs/react.production.min';
 import { useGlobalContext } from '../context'
 
 
 const Search = () => {
     const {openMenu, setOpenMenu} = useGlobalContext()
     const {openSearch, setOpenSearch} = useGlobalContext()
+    const {showResults, setShowResults} = useGlobalContext()
     const searchValue = React.useRef('')
+    const {searchTerm,setSearchTerm} = useGlobalContext()
+    const {coffeeList} = useGlobalContext()
 
-    return (
-        <div 
-            className={openSearch ? "search__container" : "search__container close"}
-        >
-            <form 
-                className="search__form"
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const searchCoffee = () => {
+        setSearchTerm(searchValue.current.value)
+        // console.log(coffeeList.slice(0,6))
+        // console.log(searchTerm)
+    }
+
+    if(searchTerm === ''){
+        setShowResults(false)
+    } else {
+        setShowResults(true)
+    }
+
+        return (
+            <div 
+                className={openSearch ? "search__container" : "search__container close"}
             >
-                <input 
-                    type="text" 
-                    id='input__name' 
-                    ref={searchValue} 
-                    placeholder="Search anything"
-                />
-            </form>
-            <div className="search__results">
-                        
-            </div>
-        </div>
+                <form 
+                    className="search__form" 
+                    onSubmit={handleSubmit}
+                >
+                    <input 
+                        type="text" 
+                        id='input__name' 
+                        ref={searchValue} 
+                        placeholder="Search anything"
+                        onChange={searchCoffee}
+                    />
+                </form>
+                <div 
+                    className={showResults ? 'search__results' : "search__results close"}
+                >
+                    {coffeeList.slice(0, 6).map((item)=> {
+                        const {
+                            id,
+                            type,
+                            index,
+                            image,
+                            title,
+                            short
+                        } =item
+                        return (
+                            <Link
+                            to={`/${type}/${id}`}
+                            key={index}
+                            className="search__link"
+                        >
+                            <div 
+                                className="img__cont"
+                                style={{ backgroundImage:`url(${image})` }}
+                            >   
+                            </div>
 
-    )
+                            <div className="search__info">
+                                <h6
+                                className="hero__tag" 
+                                >{type}</h6>
+                                <h3>{title}</h3>    
+                                {/* <p>{short}</p>     */}
+                            </div>
+                        </Link> 
+                        )
+                    })}
+                </div>
+            </div>
+    
+        )
+    
+
+
+    
 }
 
 export default Search;
--- src/context.js
+++ src/context.js
@@ -1,16 +1,35 @@
 import React, {useEffect, useState, useContext} from 'react';
+import data from './Data.json';
 
 const AppContext = React.createContext()
 
 const AppProvider = ({children}) =>{
     const [openMenu, setOpenMenu] = useState(false)
     const [openSearch, setOpenSearch] = useState(false)
+    const [searchTerm, setSearchTerm] = useState('')
+    const [coffeeList, setCoffeeList] = useState(data)
+    const [showResults, setShowResults] = useState(false)
+
+    // Search Box
+    useEffect(() => {
+    const CoffeeResults = data.filter((value) =>{
+    if(value.title.toLowerCase().includes(searchTerm.toLowerCase())){
+        return value
+      }
+    })
+    setCoffeeList(CoffeeResults)
+    },[searchTerm])
 
     return <AppContext.Provider value={{
         openMenu,
         setOpenMenu,
         openSearch,
-        setOpenSearch 
+        setOpenSearch,
+        searchTerm,
+        setSearchTerm,
+        coffeeList,
+        showResults,
+        setShowResults 
       }}>
           {children}
     </AppContext.Provider>
